8522531eeb20e308d4e2b529bf600b83f763af9e
Salvatore Allegra <salallegra@gmail.com>
2025-08-31 06:45:28 -0700

    chore(api): add EF InMemory fallback + health endpoint for Azure deploy

---
+++ TaskTracker.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTracker.Api.Controllers;
+
+[ApiController]
+[Route("")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Root() => Ok(new { ok = true, service = "TaskTracker.Api" });
+
+    [HttpGet("health")]
+    public IActionResult Health() => Ok("OK");
+}
